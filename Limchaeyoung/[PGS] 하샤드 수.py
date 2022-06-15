74504fd8fbd2a6f127b374653b5bc4793048c476
chaeyoungeee <102286483+chaeyoungeee@users.noreply.github.com>
2022-06-16 02:05:41 +0900

    Delete [PGS] 하샤드 수.py

--- Limchaeyoung/[PGS] 하샤드 수.py
+++
@@ -1,8 +0,0 @@
-# https://programmers.co.kr/learn/courses/30/lessons/12947
-def solution(x):
-    k = 0
-    n = str(x)
-    for i in n:
-        k += int(i)
-    if x % k == 0: return True
-    else: return False
