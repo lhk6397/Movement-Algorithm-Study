c8471f68c7adb6823e1a21e196c7a9f51e175bbf
maruhxn <a23686397@gmail.com>
2022-06-17 04:04:32 +0900

    [PGS] LV1 추가 문제 풀이
    과거에 풀었던 문제들

--- Kojiwan/[PGS] 오픈채팅방.py
+++ Kojiwan/[PGS] 오픈채팅방.py
@@ -14,7 +14,6 @@ def solution(records):
         else: # CHANGE
             data_storage[user_id] = user_name[0] # 저장소의 id 값에 해당하는 username을 변경
     answer = list(map(lambda x: str(data_storage[x[1]]) + x[0], answer)) # 저장소의 id에 대응하는 username으로 설정 후 문자열 출력
-    print(answer)
     return answer
 
 solution(["Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"])
---
+++ Kojiwan/[PGS] 키패드 누르기.py
@@ -0,0 +1,53 @@
+def point_length(p1, p2):
+    a = p2[0] - p1[0]
+    b = p2[1] - p1[1]
+
+    c = abs(a) + abs(b)
+    return c
+
+
+def solution(numbers, hand):
+    geometry = {
+        1: (0, 0),
+        2: (0, 1),
+        3: (0, 2),
+        4: (1, 0),
+        5: (1, 1),
+        6: (1, 2),
+        7: (2, 0),
+        8: (2, 1),
+        9: (2, 2),
+        0: (3, 1)
+    }
+
+    left_num = [1, 4, 7]
+    right_num = [3, 6, 9]
+
+    answer = ''
+    left = (3,0)
+    right = (3,2)
+    for number in numbers:
+        now_point = geometry[number]
+        if number in left_num:
+            answer += 'L'
+            left = now_point
+        elif number in right_num:
+            answer += 'R'
+            right = now_point
+        else:
+            ldist = point_length(left, now_point)
+            rdist = point_length(right, now_point)
+            if(ldist > rdist):
+                answer += 'R'
+                right = now_point
+            elif(ldist < rdist):
+                answer += 'L'
+                left = now_point
+            else:
+                if hand == "right":
+                    answer += 'R'
+                    right = now_point
+                else:
+                    answer += 'L'
+                    left = now_point
+    return answer
