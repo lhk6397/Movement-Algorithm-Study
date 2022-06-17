09a5435b7bf939b47d0bef7d152a5cae7634e0c0
Chaeyoung Lim <chaeinge@gmail.com>
2022-06-17 13:22:07 +0900

    [PGS] 완주하지 못한 선수 / LV1 / 20220613

--- Limchaeyoung/[PGS] 완주하지 못한 선수.py
+++ Limchaeyoung/[PGS] 완주하지 못한 선수.py
@@ -2,6 +2,7 @@
 def solution(participant, completion):
     participant.sort()
     completion.sort()
+
     for i in range(len(completion)):
         if participant[i] != completion[i]:
             return participant[i]
