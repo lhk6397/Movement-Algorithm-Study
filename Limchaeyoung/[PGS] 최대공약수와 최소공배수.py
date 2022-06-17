24e6c462d52885940d2c2a8c762d0edbfd2e3c90
Chaeyoung Lim <chaeinge@gmail.com>
2022-06-17 13:22:19 +0900

    [PGS] 최대공약수와 최소공배수 / LV1 / 20220616

--- Limchaeyoung/[PGS] 최대공약수와 최소공배수.py
+++ Limchaeyoung/[PGS] 최대공약수와 최소공배수.py
@@ -4,12 +4,10 @@ def solution(n, m):
     b = max(n, m)
     return [gcd(a, b), lcm(a, b)]
 
-
 def gcd(n, m):
     for x in range(n, 0, -1):
         if n % x == 0 and m % x == 0:
             return x
-
 
 def lcm(n, m):
     return (n * m) // gcd(n, m)
