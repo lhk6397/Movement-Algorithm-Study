9c36b596de9db80ac57779ba97eebc61ff4d3fd3
Chaeyoung Lim <chaeinge@gmail.com>
2022-06-17 13:21:01 +0900

    [PGS] 로또의 최고 순위와 최저 순위 / LV1 / 20220616

--- Limchaeyoung/[PGS] 로또의 최고 순위와 최저 순위.py
+++ Limchaeyoung/[PGS] 로또의 최고 순위와 최저 순위.py
@@ -3,6 +3,7 @@ def solution(lottos, win_nums):
     cnt = 0
     cnt_zero = 0
     rank = { 6:1, 5:2, 4:3, 3:4, 2:5, 1:6, 0:6 }
+
     for l in lottos:
         if l in win_nums:
             cnt += 1
