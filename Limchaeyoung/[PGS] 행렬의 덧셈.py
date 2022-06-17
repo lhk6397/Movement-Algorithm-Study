181e5cd06def42ac3313ff6022e147a112786ad5
chaeyoungeee <102286483+chaeyoungeee@users.noreply.github.com>
2022-06-17 12:26:23 +0900

    Delete [PGS] 행렬의 덧셈.py

--- Limchaeyoung/[PGS] 행렬의 덧셈.py
+++
@@ -1,7 +0,0 @@
-# https://programmers.co.kr/learn/courses/30/lessons/12950
-def solution(arr1, arr2):
-    answer = []
-    for x in range(len(arr1)):
-        plus = [(arr1[x])[i] + arr2[x][i] for i in range(len(arr1[x]))]
-        answer.append(plus)
-    return answer
