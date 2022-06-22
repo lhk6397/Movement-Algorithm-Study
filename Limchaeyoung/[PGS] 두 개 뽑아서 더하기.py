1091137e285c796e0984ac0a432d57ac153e622e
chaeyoungeee <chaeinge@gmail.com>
2022-06-23 03:21:20 +0900

    [PGS] 나머지가 1이 되는 수 찾기 / LV1 / 20220623

---
+++ Limchaeyoung/[PGS] 두 개 뽑아서 더하기.py
@@ -0,0 +1,5 @@
+# https://programmers.co.kr/learn/courses/30/lessons/68644
+from itertools import combinations
+def solution(numbers):
+    answer = list(set(x+y for x, y in combinations(numbers, 2)))
+    return sorted(answer)
