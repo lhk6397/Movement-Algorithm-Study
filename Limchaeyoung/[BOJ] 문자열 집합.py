4ccf626560e498ad47840b864cf473ad9dc9636f
chaeyoungeee <chaeinge@gmail.com>
2022-07-07 01:48:16 +0900

    [BOJ] 문자열 집합 / 실버3 / 20220707

--- Limchaeyoung/[BOJ] 문자열 집합.py
+++ Limchaeyoung/[BOJ] 문자열 집합.py
@@ -1,13 +1,11 @@
 # https://www.acmicpc.net/problem/14425
 n, m = map(int, input().split())
 cnt = 0
-S = []
-T = []
-for s in range(n):
-    S.append(input())
-for t in range(m):
-    T.append(input())
-for x in T:
-    if x in S:
+S = dict()
+for _ in range(n):
+    S[input()] = True
+for _ in range(m):
+    s = input()
+    if s in S.keys(): # 탐색 딕셔너리 사용
         cnt += 1
 print(cnt)
