81cab12986260e8fe0cd9336f250a4e8b0381d52
Chaeyoung Lim <chaeinge@gmail.com>
2022-07-14 22:53:20 +0900

    [BOJ] 대칭 차집합 / 실버3 / 20220714

--- Limchaeyoung/[BOJ] 대칭 차집합.py
+++ Limchaeyoung/[BOJ] 대칭 차집합.py
@@ -1,8 +1,9 @@
 # https://www.acmicpc.net/problem/1269
 A, B = map(int, input().split())
 a = set(map(int, input().split()))
-a2 = a.copy()
 b = set(map(int, input().split()))
+'''
+a2 = a.copy()
 b2 = b.copy()
 for x in a:
     if x in b:
@@ -11,3 +12,5 @@ for x in b:
     if x in a:
         b2.discard(x)
 print(len(a2)+len(b2))
+'''
+print(len(a-b)+len(b-a))
