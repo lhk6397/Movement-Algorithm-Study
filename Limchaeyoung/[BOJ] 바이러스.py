cb0d5cc1eccb7c2450f00ed8a695a492dc8e7d75
Chaeyoung Lim <chaeinge@gmail.com>
2022-07-15 16:16:40 +0900

    [BOJ] 바이러스 / 실버3 / 20220715

--- Limchaeyoung/[BOJ] 바이러스.py
+++ Limchaeyoung/[BOJ] 바이러스.py
@@ -2,6 +2,7 @@ import sys
 from collections import deque
 input = sys.stdin.readline
 def bfs(graph, start, visited):
+    global cnt
     queue = deque([start])
     visited[start] = True
     while queue:
@@ -10,15 +11,17 @@ def bfs(graph, start, visited):
             if not visited[g]:
                 queue.append(g)
                 visited[g] = True
+                cnt+=1
 n = int(input())
 m = int(input())
-graph = [[] for _ in range(n+1)]
+cnt = 0
+graph = {}
+for x in range(n):
+    graph[x+1] = set()
 for _ in range(m):
     a, b = map(int, input().split())
-    graph[a].append(b)
-    graph[b].append(a)
-for g in graph:
-    g.sort()
+    graph[a].add(b)
+    graph[b].add(a)
 visited = [False]*(n+1)
 bfs(graph, 1, visited)
-print(visited.count(True)-1)
+print(cnt)
