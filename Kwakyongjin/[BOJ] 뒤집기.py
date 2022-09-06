1b72e91803f0793d0be28feb8468aef12bbc73f0
Kwak yong jin <91864021+Frekion2002@users.noreply.github.com>
2022-09-06 17:37:19 +0900

    [BOJ] 뒤집기(1439번) / 실5 / 20220906

---
+++ Kwakyongjin/[BOJ] 뒤집기.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+sentence = list(map(int, input().rstrip()))
+
+tmp = sentence[0]
+count = 0
+
+for i in range(1, len(sentence)):
+    if tmp != sentence[i]:
+        count += 1
+        tmp = sentence[i]
+    else:
+        continue
+
+if count % 2 == 0:
+    print(count//2)
+
+else:
+    print((count + 1)//2)
+
+    
